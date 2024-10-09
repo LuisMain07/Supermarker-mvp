e7fc376f488f4a8bc10112ca6b8f46a997ea369d
LuisMain07 <bdmainguez00@gmail.com>
2024-10-09 00:08:27 -0500

    Avance 2 view

---
+++ Models/IPayModeRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupermarkerDefinitive.Models
+{
+    internal interface IPayModeRepository
+    {
+        void Add(PayModeModel payModeModel);
+
+        void Edit(PayModeModel payModeModel);
+
+        void Delete(int id);
+
+        IEnumerable<PayModeModel> GetAll();
+
+        IEnumerable<PayModeModel> GetByValue(string value);
+
+
+    }
+}
