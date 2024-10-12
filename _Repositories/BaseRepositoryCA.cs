2b1e8da9fe49816088bed1f3fb6e633672ebca77
LuisMain07 <bdmainguez00@gmail.com>
2024-10-12 18:33:26 -0500

    Avance 7 Categories BaseRepositoryCA

--- _Repositories/BaseRepositoryCA.cs
+++ _Repositories/BaseRepositoryCA.cs
@@ -3,14 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.Data.SqlClient;
-using Microsoft.Data;
-using SupermarkerDefinitive.Models;
-using System.Data;
 
 namespace SupermarkerDefinitive._Repositories
 {
-    internal class CategoriesRepository
+    internal class BaseRepositoryCA
     {
+        protected string connectionString;
     }
 }
--- obj/Debug/net8.0-windows/SupermarkerDefinitive.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/SupermarkerDefinitive.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SupermarkerDefinitive")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72bffc2d163ae420870fa12493f3186fe77ae0e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a87e2b5b0c091557cec0623001e238df25cd3671")]
 [assembly: System.Reflection.AssemblyProductAttribute("SupermarkerDefinitive")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SupermarkerDefinitive")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
