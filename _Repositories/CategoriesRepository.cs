4c8a29c7b2df97748172fbd23b5a90d54ce4b34d
LuisMain07 <bdmainguez00@gmail.com>
2024-10-12 18:46:05 -0500

    Avance 8 Categories clase avance CategoriesRepository

--- _Repositories/CategoriesRepository.cs
+++ _Repositories/CategoriesRepository.cs
@@ -10,7 +10,79 @@ using System.Data;
 
 namespace SupermarkerDefinitive._Repositories
 {
-    internal class CategoriesRepository
+    internal class CategoriesRepository : BaseRepositoryCA, ICategoriesRepository
     {
+        public CategoriesRepository(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public void Add(CategoriesModel categoriesModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(CategoriesModel categoriesModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<CategoriesModel> GetAll()
+        {
+            var categoriesList = new List<CategoriesModel>();
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = new SqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = "SELECT * FROM Categories ORDER BY Categories_Id DESC";
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var categoriesModel = new CategoriesModel();
+                        categoriesModel.Id = (int)reader["Categories_Id"];
+                        categoriesModel.Name = reader["Categories_Name"].ToString();
+                        categoriesModel.Description = reader["Categories_Description"].ToString();
+                        categoriesList.Add(categoriesModel);
+                    }
+                }
+            }
+            return categoriesList;
+        }
+
+        public IEnumerable<CategoriesModel> GetByValue(string value)
+        {
+            var categoriesList = new List<CategoriesModel>();
+            int categoriesId = int.TryParse(value, out _) ? Convert.ToInt32(value) : 0;
+            string categoriesName = value;
+            using (var connection = new SqlConnection(connectionString))
+            using (var cmd = connection.CreateCommand())
+            {
+                connection.Open();
+                cmd.Connection = connection;
+                cmd.CommandText = @"SELECT * FROM Categories
+                             WHERE Categories_Id=@Id or Categories_Name LIKE @Id+ '%'
+                             ORDER By Categories_Id DESC";
+                cmd.Parameters.Add("@id", SqlDbType.Int).Value = categoriesId;
+                cmd.Parameters.Add("@Name", SqlDbType.NVarChar).Value = categoriesName;
+                using (var reader = cmd.ExecuteReader())
+                {
+                    {
+                        var categoriesModel = new CategoriesModel();
+                        categoriesModel.Id = (int)reader["Customers_Id"];
+                        categoriesModel.Name = reader["Categories_Name"].ToString();
+                        categoriesModel.Description = reader["Categories_Description"].ToString();
+                        categoriesList.Add(categoriesModel);
+                    }
+                }
+            }
+            return categoriesList;
+        }
     }
 }
