a95d18aa48c70694a4385db851bbae3a7d01d18e
LuisMain07 <bdmainguez00@gmail.com>
2024-10-13 13:32:11 -0500

    Avance 3 Products interfaz IProductsView

---
+++ Views/IProductsView.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupermarkerDefinitive.Views
+{
+    internal interface IProductsView
+    {
+        string ProductsId { get; set; }
+        string ProductsName { get; set; }
+        string ProductsPrice { get; set; }
+        string ProductsStock { get; set; }
+        string ProductsCategoryId { get; set; }
+
+        string SearchValuePR { get; set; }
+        bool IsEditPR { get; set; }
+        bool IsSuccessfulPR { get; set; }
+        string MessagePR { get; set; }
+
+        event EventHandler SearchEventPR;
+        event EventHandler AddNewEventPR;
+        event EventHandler EditEventPR;
+        event EventHandler DeleteEventPR;
+        event EventHandler SaveEventPR;
+        event EventHandler CancelEventPR;
+
+        void SetProductsListBildingSource(BindingSource ProductsList);
+        void Show();
+    }
+}
--- obj/Debug/net8.0-windows/SupermarkerDefinitive.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/SupermarkerDefinitive.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SupermarkerDefinitive")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+255db025083dd49ac32234110e5dec17ecad93d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25569c01f3afed5add94efa1914c0d5dfcaf277d")]
 [assembly: System.Reflection.AssemblyProductAttribute("SupermarkerDefinitive")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SupermarkerDefinitive")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
